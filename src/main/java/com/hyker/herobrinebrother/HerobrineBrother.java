88b721d7f67329f5045d928b2a4c84f377c873c3
hyk3r <office.sild@gmail.com>
2024-09-24 03:36:07 +0200

    asd

--- src/main/java/com/hyker/herobrinebrother/HerobrineBrother.java
+++ src/main/java/com/hyker/herobrinebrother/HerobrineBrother.java
@@ -11,6 +11,6 @@ public class HerobrineBrother implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello Fabric world! Mod is Running");
 	}
 }
